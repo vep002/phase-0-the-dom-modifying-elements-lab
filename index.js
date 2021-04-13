553b89026da98d23100afbb35a01bf50ddcb9499
Victoria <vep002@gmail.com>
2021-04-13 14:06:49 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1','#victory');
+
+newHeader.id="victory";
+newHeader.innerHTML = "V is the champion";
+
